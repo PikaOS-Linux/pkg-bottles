62d2ec3cc45451dc3be0d11a036599ca763df431
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-02-24 17:29:38 +0300

    fix git

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository https://ppa.pika-os.com
 sudo add-apt-repository ppa:pikaos/pika
 sudo add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
-https://github.com/lassekongo83/adw-gtk3
+git clone https://github.com/lassekongo83/adw-gtk3
 cp -rvf ./debian ./adw-gtk3/
 cd ./adw-gtk3
 
