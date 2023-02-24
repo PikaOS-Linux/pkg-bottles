248b01bc3747368e571da71d72db910f63aafb0d
ferrreo <harderthanfire@gmail.com>
2023-02-24 15:23:09 +0000

    Fix rsync

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 dpkg-sig --sign builder ./output/adw-gtk3-theme*.deb
 
 # Pull down existing ppa repo db files etc
-rsync -azP --exclude '*.deb' ferreo@pika-os.com:/srv/www/pikappa/ ./output/repo
+rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Remove our existing package from the repo
 reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% adw-gtk3-theme*)'
@@ -11,4 +11,4 @@ reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% adw-gtk3-t
 reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/adw-gtk3-theme*.deb
 
 # Push the updated ppa repo to the server
-rsync -azP ./output/repo/ ferreo@pika-os.com:/srv/www/pikappa/
+rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
