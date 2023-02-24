22fdea7199f1de303d7e67256e2a5dc93af73ba4
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-02-24 17:39:01 +0300

    fix main.sh

--- main.sh
+++ main.sh
@@ -1,15 +1,15 @@
 # Add dependent repositories
 wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-sudo add-apt-repository https://ppa.pika-os.com
-sudo add-apt-repository ppa:pikaos/pika
-sudo add-apt-repository ppa:kubuntu-ppa/backports
+add-apt-repository https://ppa.pika-os.com
+add-apt-repository ppa:pikaos/pika
+add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 git clone https://github.com/lassekongo83/adw-gtk3
 cp -rvf ./debian ./adw-gtk3/
 cd ./adw-gtk3
 
 # Get build deps
-sudo apt-get build-dep ./
+apt-get build-dep ./ -y
 
 # Build binaries
 echo -ne 'y'  | debuild -us -uc
